3573aff1f1325c936f011db872649c2e79fe5f16
Anderson Eduardo <andersonaed@gmail.com>
2016-07-05 19:15:34 -0300

    Create poissonPMF.py

---
+++ poissonPMF.py
@@ -0,0 +1,8 @@
+#probability mass function for  Poisson distribuition
+def poissonPMF(a, b):
+    poissonPMF = []
+    mu = a
+    k = b
+    for ki in range(k):
+        poissonPMF.append( (np.exp(-mu)*mu**ki)*(math.factorial(ki))**-1 )
+    return poissonPMF
