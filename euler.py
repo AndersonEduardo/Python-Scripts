dd0f7769eefb6c8380178c0b983b859b444c4840
AndersonEduardo <andersonaed@gmail.com>
2019-01-27 11:57:19 -0300

    script para analise de histerese

--- euler.py
+++ euler.py
@@ -4,7 +4,7 @@ from scipy.integrate import odeint
 
 def meueuler( f, x0, t ):
     n = len( t )
-    x = np.array( [x0] * n )
+    x = np.array([x0] * n)
     for i in xrange( n - 1 ):
         x[i+1] = x[i] + ( t[i+1] - t[i] ) * f( x[i], t[i] )
 
@@ -12,15 +12,15 @@ def meueuler( f, x0, t ):
 
 def model2(state,t):
     x = state     
-    a = 2
+    a = 1.1
     b = 0.02
     c = 0.2
     d = 0.004
-    k = 100
+    k = 100.0
 
-    return x*a*( 1 - x*k**-1 ) # corresponds to [dx/dt, dy/dt]
+    return x*a*( 1 - x/k ) # corresponds to [dx/dt, dy/dt]
 
-x0=20
+x0 = 10.0
 
 t = np.linspace( 0, 100, 100000 )
 
---
+++ histerese.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.integrate import odeint
+
+def meueuler( f, x0, t, par ):
+    n = len(t)
+    x = np.array([x0] * n)
+    for i in xrange(n - 1):
+        x[i+1] = x[i] + (t[i+1] - t[i]) * f(x[i], t[i], par)
+
+    return x
+
+def histerese(mod, solv, x0, t, parRange):
+    definedModel = mod
+    definedSolver = solv
+    x = x0
+    time = t
+    parRange = parRange
+    histOut = []
+
+    for par_i in parRange:
+        histCalc = definedSolver(definedModel, x, time, par_i)
+        x = histCalc[len(histCalc)-1]
+        histOut.append(x)
+
+    return histOut
+
+def model(state, t, par):
+    x = state
+    a = par
+    k = 100.0
+
+    return x*a*( 1 - x/k ) # corresponds to [dx/dt, dy/dt]
+
+
+x0 = 10.0
+t = np.linspace( 0, 100, 100000 )
+a = np.linspace(0.0,2.0,30)
+
+res = histerese(model, meueuler, x0, t, a)
+
+plt.plot(a,res)
+plt.show()
