d45063a840fb43578fac1bca745f9ece2e897a0a
AndersonEduardo <andersonaed@gmail.com>
2019-03-06 14:29:12 -0300

    arquivos para haar-cascade-classifier

--- haar_cascade_classifier.py
+++ haar_cascade_classifier.py
@@ -24,7 +24,8 @@ libtiff-dev libjasper-dev libdc1394-22-dev
 sudo apt-get install libopencv-dev
 
 #5 - auxilio para obter imagens negativas (i.e., background): 
-funcao para baixar imagens por URL (URLs da imageNET, neste exemplo)
+funcao para baixar imagens por URL (URLs da imageNET, neste exemplo) - salvo como 
+arquivo dowload-image-by-link.py.
 '''
 
 import urllib.request
@@ -53,52 +54,78 @@ def store_raw_images():
         except Exception as e:
             print(str(e))  
 
-
-
 '''
-
-#funcao para baixando imagens 'negativas' no imageNet 
-# (i.e., construir um tipo de background dataset)
-import urllib.request
-import cv2
-import numpy as np
-import os
-def store_raw_images():
-    neg_images_link = 'copy the link to the URls containing the images from image-net'   
-    neg_image_urls = urllib.request.urlopen(neg_images_link).read().decode()
-    pic_num = 2393
-    
-    if not os.path.exists('neg'):
-        os.makedirs('neg')
+#6 - arquivo com a descricao das imagens negativas
+'''
+def create_pos_n_neg():
+    for file_type in ['neg']:
         
-    for i in neg_image_urls.split('\n'):
-        try:
-            print(i)
-            urllib.request.urlretrieve(i, "neg/"+str(pic_num)+".jpg")
-                        img = cv2.imread("neg/"+str(pic_num)+".jpg",cv2.IMREAD_GRAYSCALE)
-            # should be larger than samples / pos pic (so we can place our image on it)
-            resized_image = cv2.resize(img, (200, 200))
-            cv2.imwrite("neg/"+str(pic_num)+".jpg",resized_image)
-            pic_num += 1   
-        except Exception as e:
-            print(str(e)) 
-store_raw_images()
+        for img in os.listdir(file_type):
 
-#no terminal, fazer as instalacoes necessarias
-'''
-    sudo apt-get update
-    sudo apt-get upgrade
-    sudo apt-get install git
-    git clone https://github.com/Itseez/opencv.git
-    sudo apt-get install build-essential
-    sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-    sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
-    sudo apt-get install libopencv-dev
-'''
+            if file_type == 'pos':
+                line = file_type+'/'+img+' 1 0 0 50 50\n'
+                with open('info.dat','a') as f:
+                    f.write(line)
+            elif file_type == 'neg':
+                line = file_type+'/'+img+'\n'
+                with open('bg.txt','a') as f:
+                    f.write(line)
 
-#treinando o HaarCascade - terminal
 '''
-opencv_createsamples -img watch5050.jpg -bg bg.txt -info info/info.lst -pngoutput info -maxxangle 0.5 -maxyangle 0.5 -maxzangle 0.5 -num 1950
-opencv_createsamples -info info/info.lst -num 1950 -w 20 -h 20 -vec positives.vec
+#7 - criar um diretorio para as coisas da imagem positiva
+$ mkdir info 
+
+#8 - criar amostras da imagem positiva:
+opencv_createsamples -img minhaImagemPositiva.jpg -bg bg.txt -info info/info.lst -pngoutput info -maxxangle 0.5 -maxyangle 0.5 -maxzangle 0.5 -num 1950
+
+#9 - criar diretorio para dados de treinemanto
+$ mkdir data
+
+#10 - treinar o classificador cascade:
 opencv_traincascade -data data -vec positives.vec -bg bg.txt -numPos 1800 -numNeg 900 -numStages 10 -w 20 -h 20
+
+#11 - rodar o classificador (na camera, aqui):
 '''
+
+import numpy as np
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+#this is the cascade we just made. Call what you want
+watch_cascade = cv2.CascadeClassifier('watchcascade10stage.xml')
+
+cap = cv2.VideoCapture(0)
+
+while 1:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    
+    # add this
+    # image, reject levels level weights.
+    watches = watch_cascade.detectMultiScale(gray, 50, 50)
+    
+    # add this
+    for (x,y,w,h) in watches:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+
+        
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+    cv2.imshow('img',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
